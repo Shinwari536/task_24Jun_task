bc1bcacd5670f79dca8702a4320be19344a264db
Shakeel Khan <shinwari.khan474@gmail.com>
2022-06-27 11:32:01 +0500

    test cases.

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,7 +1,51 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+
+
+
+
+describe("TokenFactory", function () {
+  let erc20Contract;
+  let token;
+  let erc721Contract;
+  let nft;
+
+  let factoryContract;
+  let tokenFactory;
+
+  let owner;
+  let addr1;
+  let addr2;
+  let addr3;
+  let addr4;
+  let addr5;
+  let addr6;
+
+
+
+
+  before(async function(){
+
+    // First deploy ERC20 token and ERC721 (nft) token
+
+    // Deploying ERC20
+    erc20Contract = await ethers.getContractFactory("ERC20Token");
+    token =  await erc20Contract.deploy();
+
+    // Deploying ERC721
+    erc721Contract = await ethers.getContractFactory("NftToken");
+    nft = await erc721Contract.deploy();
+
+    // Deploying Main Factory Contract
+    factoryContract = await ethers.getContractFactory("TokenFactory");
+    [owner, addr1, addr2, addr3, addr4, addr5, addr6] = await ethers.getSigners();
+    tokenFactory = await factoryContract.deploy(nft.address, token.address);
+
+  })
+
+
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
@@ -13,7 +57,7 @@ describe("Greeter", function () {
 
     // wait until the transaction is mined
     await setGreetingTx.wait();
-
+ 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
